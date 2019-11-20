e3c94be265a59268d7654934de3c9b5ad8a4550e
Eduardo <eduardo@dudstecnologia.com.br>
2019-11-20 17:04:19 -0400

    Lista finalizada

--- src/pages/Inicio.vue
+++ src/pages/Inicio.vue
@@ -1,13 +1,18 @@
 <template>
   <q-page padding>
+    <q-input v-model="termoPesquisa" label="Pesquisar" />
 
-    <p>Total Páginas: {{ totalPaginas }}</p>
-    <p>Página Atual: {{ paginaAtual }}</p>
+    <q-card v-if="totalPaginas == 0 && statusPesquisa == false" class="my-card q-mt-sm">
+      <q-card-section>
+        Não há filmes que correspondam à sua consulta.
+      </q-card-section>
+    </q-card>
 
-    <q-infinite-scroll @load="onLoad" :offset="250">
-      <q-item v-for="f in filmes" :key="f.id" :to="{name: 'detalhes', params: { id: f.id }}" exact>
+    <q-infinite-scroll @load="onLoad" :offset="250" ref="infiniteList">
+      <q-item v-for="(f, index) in filmes" :key="index" :to="{name: 'detalhes', params: { id: f.id }}" exact>
         <q-item-section top thumbnail class="q-ml-none">
-          <img :src="`https://image.tmdb.org/t/p/w500${f.backdrop_path}`">
+          <img v-if="f.backdrop_path" :src="`https://image.tmdb.org/t/p/w500${f.backdrop_path}`">
+          <img v-if="!f.backdrop_path" src="../assets/img-not.jpg">
         </q-item-section>
 
         <q-item-section>
@@ -36,6 +41,7 @@ export default {
   data () {
     return {
       filmes: null,
+      termoPesquisa: '',
       paginaAtual: 0,
       totalPaginas: 0,
       resultado: 0,
@@ -44,71 +50,66 @@ export default {
   },
   created () {
     this.$emit('altera-titulo', 'Filmes Populares')
-    // if(this.paginaAtual == 0) {
-    //   this.getFilmes(null, 1)
-    // }
+    if(this.paginaAtual == 0) {
+      this.getFilmes(null, 1)
+    }
   },
   methods: {
-    getFilmes (pesquisa, pagina, done) {
+    getFilmes (pesquisa, pagina) {
       this.$emit('progresso', true)
 
       console.log("Entrou aqui, pagina: " + pagina)
 
       this.statusPesquisa = true;
 
-      let url = '/discover/movie?sort_by=popularity.desc&api_key=374e1f09ba996d9840f71bac5d4e527f&language=pt-BR';
-
-      // if(pesquisa) {
-      //   var url = `/discover/movie?sort_by=popularity.desc&api_key=374e1f09ba996d9840f71bac5d4e527f&language=pt-BR&page=${pagina}&query=${pesquisa}`
-      // } else {
-      //   var url = `/discover/movie?sort_by=popularity.desc&api_key=374e1f09ba996d9840f71bac5d4e527f&language=pt-BR&page=${pagina}`
-      // }
-
       if(pesquisa) {
-        url += `&page=${pagina}&query=${pesquisa}`
+        var url = `/search/movie?api_key=374e1f09ba996d9840f71bac5d4e527f&language=pt-BR&page=${pagina}&query=${pesquisa}`
       } else {
-        url += `&page=${pagina}`
+        var url = `/discover/movie?sort_by=popularity.desc&api_key=374e1f09ba996d9840f71bac5d4e527f&language=pt-BR&page=${pagina}`
       }
 
-      // this.$axios.get(`/discover/movie?sort_by=popularity.desc&api_key=374e1f09ba996d9840f71bac5d4e527f&language=pt-BR&page=${pagina}`)
       this.$axios.get(url)
         .then((res) => {
           console.log(res.data)
 
-          // this.filmes = res.data.results
-          if(this.paginaAtual == 0) {
+          if (pesquisa || pagina == 1) {
             this.filmes = res.data.results
           } else {
             this.filmes = this.filmes.concat(res.data.results)
           }
-          // this.filmes = this.filmes.concat(res.data.results)
 
           this.paginaAtual = res.data.page
           this.totalPaginas = res.data.total_pages
 
-          this.$emit('progresso', false)
           this.statusPesquisa = false
-          done()
+          this.$emit('progresso', false)
         })
         .catch((err) => {
           console.log(err)
-          this.$emit('progresso', false)
+
           this.statusPesquisa = false
-          done()
+          this.$emit('progresso', false)
         });
     },
     detalhesFilme(id) {
       console.log('Selecionou: ' + id)
     },
     onLoad (index, done) {
-      console.log('Está no onload')
-      // console.log(done);
-      // done()
-      if(this.paginaAtual == 0) {
-        this.getFilmes(null, 1, done)
-      } else if(!this.statusPesquisa) {
+      if(!this.statusPesquisa) {
         this.paginaAtual++
-        this.getFilmes(null, this.paginaAtual, done)
+        this.getFilmes(null, this.paginaAtual)
+      }
+    done()
+    }
+  },
+  watch: {
+    termoPesquisa () {
+      if(this.termoPesquisa.length > 2) {
+        this.$emit('altera-titulo', `Pesquisa '${this.termoPesquisa}'`)
+        this.getFilmes (this.termoPesquisa, 1)
+      } else if(this.termoPesquisa.length == 0) {
+        this.$emit('altera-titulo', 'Filmes Populares')
+        this.getFilmes(null, 1)
       }
     }
   }
